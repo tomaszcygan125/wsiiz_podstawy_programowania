553a8f3911bbec6e8bc395a884def248886bd0c9
TomaszCygan <t.cygan@onet.pl>
2024-10-28 18:09:15 +0100

    corrections of zad4

--- lab2/zad4.py
+++ lab2/zad4.py
@@ -1,10 +1,59 @@
-gol = float(input("podaj gole: "))
+gol = int(input("podaj gole: "))
 
-bonus = float(input("podaj bonus: "))
+bonus = 0.0
 
-punkty = gol * 10
+# 0
+for x in range(gol):
+    bonus += 10
 if gol > 10:
-    punkty += 10
+    bonus += 10
 elif gol > 5:
-    punkty += 5
+    bonus += 5
+
+print("calkowity wynik: ", bonus)
+
+
+# a
+gol = int(input("podaj gole: "))
+
+bonus = 0.0
+for x in range(gol):
+    bonus += 10
+if gol >= 5:
+    bonus += 5
+
+if gol > 10:
+    bonus += 10
+
+print("calkowity wynik: ", bonus)
+
+# a
+gol = int(input("podaj gole: "))
+
+bonus = 0.0
+for x in range(gol):
+    bonus += 10
+if gol >= 5:
+    bonus += 5
+
+if gol > 10:
+    bonus += 10
+
+print("calkowity wynik: ", bonus)
+
+# b
+gol = int(input("podaj gole: "))
+
+bonus = 0.0
+for x in range(gol):
+    bonus += 10
+if gol > 10:
+    bonus += 10
+    bonus +=5
+
+print("calkowity wynik: ", bonus)
+
+
+
+
 
