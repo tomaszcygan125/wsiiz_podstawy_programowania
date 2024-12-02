59587d33ab48a75c47cfa3ec8a2295824258c85e
TomaszCygan <t.cygan@onet.pl>
2024-12-02 18:51:58 +0100

    finishing the 4-1

--- lab4/zad4.py
+++ lab4/zad4.py
@@ -1,11 +1,20 @@
-zdanie  = input('podaj zdanie: ')
+import string
 
-lista = []
-for litera in zdanie:
-    if litera.isalpha():
-        lista.append(litera.upper())
+zdanie = input("podaj zdanie: ")
 
+import string
+litery = []
+for letter in zdanie:
+    if letter.isalpha():
+        if letter not in litery:
+            litery.append(letter.lower())
 
+litery_sort = sorted(litery)
 
+print(litery_sort)
 
-print(lista)
+alfabet = list(string.ascii_lowercase)
+
+for litera in alfabet:
+    if litera not in litery:
+        print(litera, end=', ')
---
+++ lab4/zad6.py
@@ -0,0 +1,6 @@
+owoce = ('jabłko', 'banan', 'gruszka', 'banan', 'banan', 'malina')
+
+
+ilosc_banan = owoce.count('banan')
+
+print(ilosc_banan)
